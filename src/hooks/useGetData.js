62279ebc921d26df13264be3beaea88576c90741
Joel Valdivia <jooelvaldivi@gmail.com>
2020-05-19 14:21:21 -0500

    [CHANGE] cambio de url http a https

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,5 +1,5 @@
 import { useState, useEffect} from 'react';
-const api = 'http://us-central1-gndx-cv.cloudfunctions.net/me';
+const api = 'https://us-central1-gndx-cv.cloudfunctions.net/me';
 
 const useGetData = () => {
 
